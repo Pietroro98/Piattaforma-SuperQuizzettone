2ba8d75b3029552400c618ec2575489d65277919
Pietro Romano <pietroro98@gmail.com>
2026-04-27 14:37:36 +0200

    primo commit su dev

---
+++ src/test/java/com/superquizzettone/SuperquizzettoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superquizzettone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperquizzettoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
