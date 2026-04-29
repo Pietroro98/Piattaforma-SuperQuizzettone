06a4a5ba0783d54d49f4a85d5425077238a29a1f
Mohamed Mousa <moimousa03@gmail.com>
2026-04-29 15:48:46 +0200

    Fix DTOs e aggiornamento repository e services di Question e Answer

--- backend/src/main/java/com/superquizzettone/dto/QuestionDTO.java
+++ backend/src/main/java/com/superquizzettone/dto/QuestionDTO.java
@@ -1,4 +1,51 @@
 package com.superquizzettone.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.superquizzettone.model.Category;
+import com.superquizzettone.model.Question;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@NoArgsConstructor
 public class QuestionDTO {
+
+    private Long id;
+    private String description;
+    private List<AnswerDTO> answers;
+    private Category category;
+    private String tag;
+    private String motivationRejection;
+
+
+    public static QuestionDTO buildQuestionDTOFromModel(Question questionModel){
+
+        QuestionDTO result = new QuestionDTO();
+        result.setId(questionModel.getId());
+        result.setDescription(questionModel.getDescription());
+        result.setCategory(questionModel.getCategory());
+        result.setTag(questionModel.getTag());
+        result.setMotivationRejection(questionModel.getMotivationRejection());
+
+        if (questionModel.getAnswers() != null) {
+            result.setAnswers(
+                    questionModel.getAnswers()
+                            .stream()
+                            .map(AnswerDTO::buildAnswerDTOfromModel)
+                            .collect(Collectors.toList())
+            );
+        } else {
+            result.setAnswers(Collections.emptyList());
+        }
+
+        return result;
+    }
+
 }
