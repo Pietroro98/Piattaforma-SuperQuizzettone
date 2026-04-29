2db2c07a2eec1ec3882a2a65acf156723526dc8d
AlexanderT29 <alextesoro96@gmail.com>
2026-04-29 17:19:29 +0200

    Aggiunta funzionalità Quiz

--- backend/src/main/java/com/superquizzettone/dto/QuizDTO.java
+++ backend/src/main/java/com/superquizzettone/dto/QuizDTO.java
@@ -1,4 +1,64 @@
 package com.superquizzettone.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.superquizzettone.model.Question;
+import com.superquizzettone.model.Quiz;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@AllArgsConstructor
 public class QuizDTO {
+
+    private Long id;
+
+    @NotBlank(message="La descrizione non può essere nulla")
+    private String description;
+
+    @NotBlank(message = "Il nome non può essere nullo")
+    private String name;
+
+    @NotBlank(message = "Il tempo del quiz non può essere nullo")
+    private String quizTime;
+
+    private Double totalPoints;
+
+    private List<Long> questions = new ArrayList<>();
+
+    public QuizDTO(){}
+
+    public static QuizDTO buildDTOFromModel(Quiz model){
+        QuizDTO result = new QuizDTO(
+                model.getId(),
+                model.getDescription(),
+                model.getName(),
+                model.getQuizTime(),
+                model.getTotalPoints(),
+                model.getQuestions()
+                        .stream()
+                        .map(Question::getId)
+                        .toList()
+        );
+        return result;
+    }
+
+    public static Quiz buildModelFromDTO(QuizDTO dto) {
+        Quiz result = new Quiz();
+        result.setDescription(dto.getDescription());
+        result.setName(dto.getName());
+        result.setQuizTime(dto.getQuizTime());
+        result.setTotalPoints(dto.getTotalPoints());
+        return result;
+    }
+
+
+
 }
--- backend/src/main/java/com/superquizzettone/service/quiz/QuizService.java
+++ backend/src/main/java/com/superquizzettone/service/quiz/QuizService.java
@@ -1,4 +1,27 @@
 package com.superquizzettone.service.quiz;
 
+import com.superquizzettone.dto.QuizDTO;
+import com.superquizzettone.model.Quiz;
+
+import java.util.List;
+
 public interface QuizService {
+
+    public Quiz insertNew(Quiz quiz);
+
+    public List<Quiz> listAllQuiz();
+
+    public Quiz update(Quiz newQuiz);
+
+    public void delete(Long quizId);
+
+    public Quiz getQuiz(String quizName);
+
+    public Quiz getQuizById(Long quizId);
+
+    public List<Quiz> findQuizByExample(QuizDTO quizExample);
+
+    public List<Quiz> findQuizsByName(String quizName);
+
+
 }
