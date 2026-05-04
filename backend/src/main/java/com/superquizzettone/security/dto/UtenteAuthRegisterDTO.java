b5716c77a850998c79bfff615171381639db25e8
Tommy2001-web <tommy.lenzi@gmail.com>
2026-05-04 15:28:35 +0200

    fix al data init

--- backend/src/main/java/com/superquizzettone/security/dto/UtenteAuthRegisterDTO.java
+++ backend/src/main/java/com/superquizzettone/security/dto/UtenteAuthRegisterDTO.java
@@ -18,9 +18,13 @@ public class UtenteAuthRegisterDTO {
     @NotBlank(message = "Il nome non può essere nullo, vuoto o contenere solo spazi")
     private String name;
 
-    @NotBlank(message = "Il nome non può essere nullo, vuoto o contenere solo spazi")
+    @NotBlank(message = "Il cognome non può essere nullo, vuoto o contenere solo spazi")
     @Size(min = 4, message = "Lo username deve avere almeno 4 caratteri")
     private String surname;
+
+    @NotBlank(message = "non può essere nullo, vuoto o contenere solo spazi")
+
+    private String username;
 
     @NotBlank(message = "Il nome non può essere nullo, vuoto o contenere solo spazi")
     @Size(min = 6, max = 12, message = "La password deve avere tra 6 e 12 caratteri")
@@ -28,9 +32,6 @@ public class UtenteAuthRegisterDTO {
             regexp = "^(?=.*[A-Z])(?=.*[a-zA-Z0-9])(?=.*[!£&$?_-])[A-Za-z0-9!£&$?_-]{6,12}$",
             message = "La password deve contenere almeno una maiuscola, caratteri alfanumerici e un carattere speciale (!£&$?_-)"
     )
-    private String username;
-
-    @NotBlank(message = "Il nome non può essere nullo, vuoto o contenere solo spazi")
     private String password;
 
     public User buildUtenteModel() {
