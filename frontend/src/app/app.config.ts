c9474b45349037753243cfe9664f39ec963ee127
Alionor <elegrisa32@libero.it>
2026-05-05 09:20:13 +0200

    risolti import errati

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './authentication/auth/auth-interceptor.interceptor';
+import { authInterceptor } from './features/authentication/auth/auth-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
