022a4f90bdd6269751297377d55b5d6547e6fd0e
Pietro Romano <pietroro98@gmail.com>
2026-05-15 12:58:16 +0200

    aggiornamento

--- frontend/src/app/core/service/question.service.ts
+++ frontend/src/app/core/service/question.service.ts
@@ -1,35 +1,36 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { StorageService } from "./storage.service";
-import { Question } from "../models/question.model";
-import { map, Observable } from "rxjs";
-import { Category } from "../models/quiz.model";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { StorageService } from './storage.service';
+import { Question } from '../models/question.model';
+import { map, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class QuestionService {
-
   private readonly http = inject(HttpClient);
   private readonly storage = inject(StorageService);
   baseUrl: string = 'http://localhost:8080/superQuizzettone/api';
 
-
   createQuestion(payload: Question): Observable<any> {
-    return this.http.post<Question>(`${this.baseUrl}/writer/create-question`, payload);
+    return this.http.post<Question>(
+      `${this.baseUrl}/writer/create-question`,
+      payload,
+    );
   }
 
   saveDraft(payload: Question): Observable<any> {
-    return this.http.post<Question>(`${this.baseUrl}/writer/save-draft`, payload);
+    return this.http.post<Question>(
+      `${this.baseUrl}/writer/save-draft`,
+      payload,
+    );
   }
 
   getAllCategory(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/open-controller/categories-list`)
-     .pipe(
-          map((response) => response.data));
+    return this.http
+      .get<any>(`${this.baseUrl}/open-controller/categories-list`)
+      .pipe(map((response) => response.data));
   }
 
-getQuestionsInReview() : Observable<any> {
-  return this.http.get(`${this.baseUrl}/open-controller/all-questions`);
-}
-
-
+  getQuestionsInReview(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/open-controller/all-questions`);
+  }
 }
