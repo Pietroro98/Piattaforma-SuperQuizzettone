faede8c1e1859a2c394bcdfb6160e6fb47461821
Pietro Romano <pietroro98@gmail.com>
2026-05-15 12:50:32 +0200

    aggiornamento iniziale pagina backoffice / home

--- frontend/src/app/layout/sidenav/sidenav.component.ts
+++ frontend/src/app/layout/sidenav/sidenav.component.ts
@@ -1,17 +1,61 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../core/service/auth.service';
 import { NgIf } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+interface MenuItem {
+  label: string;
+  route: string;
+  roles?: string[];
+}
 
 @Component({
   selector: 'app-sidenav',
-  imports: [NgIf],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
   @Input() isOpen = false;
   @Output() close = new EventEmitter<void>();
 
-  onClose() {
+  constructor(private authService: AuthService) {}
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Home',
+      route: '/home',
+    },
+    {
+      label: 'Play',
+      route: '/play',
+      roles: ['ROLE_PLAYER'],
+    },
+    {
+      label: 'Profilo',
+      route: '/profile',
+    },
+    {
+      label: 'Backoffice',
+      route: '/backoffice',
+      roles: ['ROLE_ADMINISTRATOR', 'ROLE_REVIEWER','ROLE_ERITER'],
+    },
+  ];
+
+  onClose(): void {
     this.close.emit();
+  }
+
+  canShowItem(item: MenuItem): boolean {
+    if (!item.roles || item.roles.length === 0) {
+      return true;
+    }
+
+    return this.authService.hasAnyRole(item.roles);
+  }
+
+  logout(): void {
+    this.authService.logout();
+    window.location.href = '/login';
   }
 }
